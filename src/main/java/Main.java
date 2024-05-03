607c4bbaa6a5ce50dba2824150a4259185e18330
Tobias <tobidelmo@gmail.com>
2024-05-03 14:47:36 -0300

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.OutputStream;
+import java.io.*;
 
 
 public class Main {
@@ -19,9 +20,23 @@ public class Main {
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+       BufferedReader in = new BufferedReader(
+    	          new InputStreamReader(clientSocket.getInputStream()));
        OutputStream outStream = clientSocket.getOutputStream();
-       String response = "HTTP/1.1 200 OK\r\n\r\n";
+       String response = null;
+       String line = in.readLine();
+       String[] parts = line.split(" ");
+       String method = parts[0];
+       String path = parts[1];
+       
+       if (path.equals("/")) {
+    	   response = "HTTP/1.1 200 OK\r\n\r\n"
+       }
+       else {
+    	   response ="HTTP/1.1 404 Not Found\r\n\r\n";
+       }
        outStream.write(response.getBytes());
+       
        
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
